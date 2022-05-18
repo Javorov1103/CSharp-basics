0e9f1bc76d2ae01eb22511dbaa560d7409c1609f
Kalin Javorov <k.javorov@gmail.com>
2022-05-18 15:06:32 +0300

    string builder

---
+++ StringsDemo/StringBuilderDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.WriteLine(list);
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append("Hello. ");
+            builder.AppendLine("My name is Kalin");
+            builder.AppendLine("new row");
+
+            Console.WriteLine(builder);
+
+            string text = builder.ToString();
+
+            builder.Clear();
+
+            //string text = "Hello.";
+            //text += " My name is Kalin";
+            //text += " \nnew row";
+
+            //Console.WriteLine(text);
+        }
+    }
+}
---
+++ StringsDemo/Task 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string forbidenWords = "C#, CLR, Microsoft-NovaDuma@test";
+
+            string[] words = forbidenWords.Split(new char[] { ',',' ','-','@' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string text = "Microsoft announced its next generation C# compiler today. It uses advanced parser and special optimizer for the Microsoft CLR.";
+
+            foreach (var x in words)
+            {
+                int lenght = x.Length;
+                text = text.Replace(x, "*");
+            }
+
+            Console.WriteLine(text);
+
+
+        }
+    }
+}
