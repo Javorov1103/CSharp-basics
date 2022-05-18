519fdd0b68dc93085683279818755c89c9c8091e
Kalin Javorov <k.javorov@gmail.com>
2022-05-18 15:07:19 +0300

    added few projects

---
+++ Factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("n = ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            decimal factorial = Factorial(n);
+
+            Console.WriteLine("{0}! = {1}", n, factorial);
+        }
+
+        static decimal Factorial(int n)
+        {
+            // The bottom of the recursio
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            // Recursive call: the method calls itself
+            else
+            {
+                int nextNumber = n - 1;
+                return n * Factorial(nextNumber);
+            }
+
+        }
+    }
+}
