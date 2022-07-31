4df9c0d1ea8d63db4bfed17eb03cb618a0bba7b9
Kalin Javorov <k.javorov@gmail.com>
2022-07-31 20:56:54 +0300

    workshop initial

---
+++ Workshop-Skeleton/Forum.App/Factories/CommandFactory.cs
@@ -0,0 +1,45 @@
+﻿namespace Forum.App.Factories
+{
+	using Contracts;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class CommandFactory : ICommandFactory
+	{
+		private IServiceProvider serviceProvider;
+
+        public CommandFactory(IServiceProvider serviceProvider)
+        {
+			this.serviceProvider = serviceProvider;
+        }
+		public ICommand CreateCommand(string commandName)
+		{
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			Type[] typesInAssembly = assembly.GetTypes();
+			Type commandType = typesInAssembly.FirstOrDefault(t => t.Name == commandName + "Command");
+
+			if(commandType == null)
+            {
+				throw new ArgumentException("Command not found!");
+            }
+
+			if(!typeof(ICommand).IsAssignableFrom(commandType))
+            {
+				throw new ArgumentException($"{commandType} is not a command");
+			}
+
+			ParameterInfo[] ctorParams = commandType.GetConstructors().First().GetParameters();
+			object[] args = new object[ctorParams.Length];
+
+            for (int i = 0; i < args.Length; i++)
+            {
+				args[i] = this.serviceProvider.GetService(ctorParams[i].ParameterType);
+            }
+
+			ICommand command = (ICommand)Activator.CreateInstance(commandType, args);
+
+			return command;
+		}
+	}
+}
