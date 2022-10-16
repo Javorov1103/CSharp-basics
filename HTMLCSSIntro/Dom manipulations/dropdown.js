68425485f701b8d8a531039173180ff53c80fae0
Kalin Javorov <k.javorov@gmail.com>
2022-10-16 12:22:16 +0300

    dom manipulations

---
+++ HTMLCSSIntro/Dom manipulations/dropdown.js
@@ -0,0 +1,23 @@
+function addItem() {
+//Select the two inputs
+var input1= document.getElementById("newItemText")
+var input2 = document.getElementById("newItemValue")
+var textInput = input1.value;
+var valueInput = input2.value;
+
+//Create an option element
+var optionEl = document.createElement("option");
+
+//Set the attr of the new option element
+optionEl.setAttribute('value', valueInput);
+optionEl.innerHTML = textInput;
+
+//Add the new option element into the select box
+document.getElementById('menu').appendChild(optionEl)
+
+//Clear the inputs
+input1.value ='';
+input2.value='';
+}
+
+
---
+++ HTMLCSSIntro/Dom manipulations/subtract.js
@@ -0,0 +1,8 @@
+function subtract() {
+   var firstNumber = document.getElementById('firstNumber').value;
+   var secondNumber = document.getElementById('secondNumber').value;
+
+   var result = firstNumber - secondNumber;
+
+   document.getElementById('result').innerHTML = result;
+}
